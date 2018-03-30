ff5c00f9f88e4c863704b18fa858b02e0d1bb783
bklynate <nathanielcfa@gmail.com>
2018-03-30 03:30:28 -0400

    heroku

--- config/keys.js
+++ config/keys.js
@@ -5,6 +5,7 @@ const devKeys = require('./dev');
 const { NODE_ENV } = process.env;
 
 // if the environment is production, load prod keys
+
 if (NODE_ENV === "production") prodKeys; // eslint-disable-line
 
 export default devKeys;
