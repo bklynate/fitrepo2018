b7bc97b791fe7330ff2e9aeccc78dea1db513162
bklynate <nathanielcfa@gmail.com>
2018-03-31 16:25:09 -0400

    default macros start at 0

--- models/User.js
+++ models/User.js
@@ -5,9 +5,9 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   googleID: String,
   macros: {
-    proteinCount: { type: Number, default: 500 },
-    carbCount: { type: Number, default: 500 },
-    fatCount: { type: Number, default: 500 }
+    proteinCount: { type: Number, default: 0 },
+    carbCount: { type: Number, default: 0 },
+    fatCount: { type: Number, default: 0 }
   }
 });
 
